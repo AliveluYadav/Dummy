0a8e947dacf27e7433ea66bfa0896eb79bffc2f3
Alivelu <alivelu7193@gmail.com>
2020-01-06 19:14:58 +0530

    java

---
+++ src/com/sample/new1/Alivelu.java
@@ -0,0 +1,15 @@
+package com.sample.new1;
+
+public class Alivelu {
+
+	 public static void main(String[] args) {
+	        
+	      int num1 = 5, num2 = 25, sum;
+	      sum = num1 + num2;
+
+	      System.out.println("Sum of these numbers: "+sum);
+	   }
+
+	}
+
+
